08464f007ca6ce0be69e4c9f73761da55c3de5ef
Olzhas Aitkaliyev <o.aitkaliyev@innopolis.ru>
2017-04-10 18:25:05 +0300

    Further improvements. Heat loss now working

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,21 +1,104 @@
 package sample;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+import java.util.DoubleSummaryStatistics;
 
 public class Controller {
-    ObservableList<String> dimensionList = FXCollections.observableArrayList("DN 15", "DN 20");
 
     @FXML
     private ChoiceBox dimensionBox;
 
     @FXML
-    void onSubmitClick(ActionEvent event) {
+    private ChoiceBox lengthBox;
 
-        System.out.println(dimensionBox.getValue());
+    @FXML
+    private ChoiceBox soilBox;
+
+    @FXML
+    private TextField distanceField;
+
+    @FXML
+    private TextField mediumTempField;
+
+    @FXML
+    private TextField surfaceTempField;
+
+
+    @FXML
+    void onSubmitClick(ActionEvent event) {
+        double distance = Double.parseDouble(distanceField.getText());
+        double mediumT = Double.parseDouble(mediumTempField.getText());
+        double surfaceT = Double.parseDouble(surfaceTempField.getText());
+
+        double mediumTK = celsiusToKelvin(mediumT);
+        double surfaceTK = celsiusToKelvin(surfaceT);
+
+        String dimension = (String) dimensionBox.getValue();
+
+        PipeFactory pipeFactory = new PipeFactory();
+        Pipe standardSinglePipe = pipeFactory.makeStandardSinglePipe(dimension);
+
+        double a = Double.parseDouble((String)lengthBox.getValue()) / 1000;
+
+        double ltrInsulation = standardSinglePipe.getLinearThermalResistance();
+
+        String soil = (String) soilBox.getValue();
+        double ltrGround = ltrGround(lambdaGround(soil), a, distance, standardSinglePipe.getExternalDiameter());
+
+        double heatLoss = heatLoss(mediumTK, surfaceTK, ltrInsulation, ltrGround);
+
+        System.out.println(heatLoss);
+
+    }
+
+    //dN is the square cross section of the outer most layer (the soil) considered with an equivalent diameter calculated by Equation (6)
+    public double dN(double a) {
+        return 1.073 * a;
+    }
+
+    public double celsiusToKelvin(double temp) {
+        return 274.15 + temp;
+    }
+
+    //hE is the distance between the centre of the pipe and the ground surface [m] calculated by Equation (5)
+    public double hE(double h, double dE) {
+        return h + dE / 2;
+    }
+
+    public double ltrGround(double lambdaE, double a, double h, double dE) {
+        double dN = dN(a);
+        double hE = hE(h, dE);
+        return (1 / (2 * Math.PI * lambdaE)) * 1 / (Math.cosh(2 * hE / dN));
+    }
+
+    public double lambdaGround(String type) {
+        double lambda;
+        switch (type) {
+            case "Dry":
+                lambda = 0.92;
+                break;
+
+            case "Frozen":
+                lambda = 0.93;
+                break;
+
+            case "Saturated with water":
+                lambda = 0.95;
+                break;
+
+            default:
+                lambda = 0.92;
+                break;
+        }
+        return lambda;
+    }
+
+    public double heatLoss(double mediumT, double surfaceT, double ltrInsulation, double ltrGround) {
+        return (mediumT + surfaceT) / (ltrInsulation + ltrGround);
     }
 
 }
